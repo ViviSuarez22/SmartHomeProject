3f9de37fc21aca5c49d3e8098dd68d9689b18d1a
ViviSuarez22 <vivisuarez22@hotmil.com>
2025-04-06 17:24:52 -0500

    SmartDevice Class

--- src/SmartDevice.java
+++ src/SmartDevice.java
@@ -1,2 +1,19 @@
-public class SmartDevice {
+public abstract class SmartDevice {
+    String brand;
+    String model;
+    String status;
+
+    public SmartDevice (String brand, String model, String status){
+    this.brand = brand;
+    this.model = model;
+    this.status = status;
+    }
+
+    public void turnOn(){
+
+    }
+    public void turnOff(){
+
+    }
+    public abstract void deviceInfo();
 }
